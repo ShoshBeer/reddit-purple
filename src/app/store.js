a7ed8704465c5a8ce966705d73d183d6b3a3a237
ShoshBeer <shoshanabeer@gmail.com>
2022-12-05 17:35:29 -0500

    create links slice and addLinks action

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import { linksReducer } from '../features/Components/FindLinksToReddit/FindLinksToRedditSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    links: linksReducer
   },
 });
