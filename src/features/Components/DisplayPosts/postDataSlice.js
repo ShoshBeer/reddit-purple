4b48c1219e798d4f1a846bd346e54b4896c26767
JeanJoblin <invisibleorbs@gmail.com>
2022-12-05 17:28:01 -0500

    start making postDataSlice

---
+++ src/features/Components/DisplayPosts/postDataSlice.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+const initalState = { foundPosts: [
+    'https://www.reddit.com/r/AmItheAsshole/comments/zd31mr/aita_for_letting_my_sister_puke_on_my_brother_in/',
+    'https://www.reddit.com/r/AmItheAsshole/comments/zd62bi/aita_for_making_my_daughter_return_a_halloween/',
+    'https://www.reddit.com/r/AmItheAsshole/comments/zd5vpq/aita_for_telling_my_parents_i_wont_attend_their/',
+]}
+
+// export const fetchURLData = createAsyncThunk('postData/fetchURLData', async () => {
+//     state.foundPosts.map((post) => {
+//         const response = await fetch(state.postJSON)
+//     })
+// })
+
+const postDataSlice = createSlice({
+    name: 'postData',
+    initialState,
+    reducers: {
+        loadJSON(state) {
+            state.postJSON = state.foundPosts.map(post => post + '.json');
+        },
+        loadTitles(state) {
+            state.titles = state.foundPosts.map(post => post.data.children[0].data.title)
+        }
+    },
+    extraReducers: builder => {
+        builder
+            .addCase()
+    }
+})
+
+export default postDataSlice.reducer;
