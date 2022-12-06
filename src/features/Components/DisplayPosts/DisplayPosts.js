56e894850dd79491a68d3934a1b67e78125d8d4f
JeanJoblin <invisibleorbs@gmail.com>
2022-12-06 13:41:20 -0500

    grab titles from json

--- src/features/Components/DisplayPosts/DisplayPosts.js
+++ src/features/Components/DisplayPosts/DisplayPosts.js
@@ -1,14 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { FindLinksToReddit } from "../FindLinksToReddit/FindLinksToReddit";
 import { Post } from "../Post/Post";
+import { selectPosts, selectTitles, loadTitles, loading} from "./postDataSlice"
 
 export function DisplayPosts() {
-
+    const dispatch = useDispatch();
     const [links, setLinks] = useState([
         'https://www.reddit.com/r/AmItheAsshole/comments/zd31mr/aita_for_letting_my_sister_puke_on_my_brother_in/',
         'https://www.reddit.com/r/AmItheAsshole/comments/zd62bi/aita_for_making_my_daughter_return_a_halloween/',
         'https://www.reddit.com/r/AmItheAsshole/comments/zd5vpq/aita_for_telling_my_parents_i_wont_attend_their/',
     ]);
+    
+    const titles = useSelector(selectTitles);
 
     return (
         <div className="post-container">
