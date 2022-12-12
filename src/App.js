9a610e3d1ba6c1aca55ac9549d9998d410f6b459
ShoshBeer <shoshanabeer@gmail.com>
2022-12-12 15:31:30 -0500

    create sidebar component and adjust responsiveness

--- src/App.js
+++ src/App.js
@@ -11,28 +11,55 @@ import search from './resources/search.jpg';
 
 function App() {
   return (
-    <Container fluid className="App border">
-      <Row>
-        <Col>
-          <h1>Welcome to the Museum of Reddit Natural History!</h1>
-          <p>Use this site to browse Reddit posts linked in the comments of another post!</p>
-          <p>Pop this link below to see what happens: https://www.reddit.com/r/AskReddit/comments/9wsvhk/what_is_the_best_post_of_reddit_of_all_time/</p>
-        </Col>
-      </Row>
-      <Row>
-        <Col xs="1"  className='border'>
-         <Sidebar shortcuts={[{title: 'help', src: help, alt: 'Click here to go back to the search page.', post: null}, {title: 'search', src: search, alt: 'Click here to search for a new post.', post: null}]}/> 
-        </Col>
-        <Col xs="1">
-         <Sidebar shortcuts={[{title: 'title', src: 'https://i.redd.it/h6grhudubv4a1.jpg', alt: 'birbs', post: {comments: {top: "whos commenting in 2019?"}, postNum: 1}}]}/> 
-        </Col>
-        <Col>
-          <InputField mt="10"/>
-          <DisplayPosts />
-        </Col>
-      </Row>
-    </Container>
+    <div className='main-wrapper main-wrapper-responsive-lg flex-container'>
+      <Sidebar 
+        className='flex-child'
+        shortcuts={[{title: 'help', src: help, alt: 'Click here to go back to the search page.', post: null}, {title: 'search', src: search, alt: 'Click here to search for a new post.', post: null}]}/>
+      <Sidebar 
+        className='flex-child'
+        shortcuts={[{title: 'title', src: 'https://i.redd.it/h6grhudubv4a1.jpg', alt: 'birbs', post: {comments: {top: "whos commenting in 2019?"}, postNum: 1}}, {title: 'othertitle', src: "https://static.thenounproject.com/png/180007-200.png", alt: "sub", post: {}}]}/> 
+      <Container 
+        fluid 
+        className="flex-child main App border">
+        <Row>
+          <Col>
+            <h1>Welcome to the Museum of Reddit Natural History!</h1>
+            <p>Use this site to browse Reddit posts linked in the comments of another post!</p>
+            <p>Pop this link below to see what happens: https://www.reddit.com/r/AskReddit/comments/9wsvhk/what_is_the_best_post_of_reddit_of_all_time/</p>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <InputField mt="10"/>
+            <DisplayPosts />
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 }
 
 export default App;
+
+
+{/* <Container fluid className="App border">
+<Row>
+  <Col>
+    <h1>Welcome to the Museum of Reddit Natural History!</h1>
+    <p>Use this site to browse Reddit posts linked in the comments of another post!</p>
+    <p>Pop this link below to see what happens: https://www.reddit.com/r/AskReddit/comments/9wsvhk/what_is_the_best_post_of_reddit_of_all_time/</p>
+  </Col>
+</Row>
+<Row>
+  <Col xs="1"  className='border'>
+   <Sidebar shortcuts={[{title: 'help', src: help, alt: 'Click here to go back to the search page.', post: null}, {title: 'search', src: search, alt: 'Click here to search for a new post.', post: null}]}/> 
+  </Col>
+  <Col xs="1">
+   <Sidebar shortcuts={[{title: 'title', src: 'https://i.redd.it/h6grhudubv4a1.jpg', alt: 'birbs', post: {comments: {top: "whos commenting in 2019?"}, postNum: 1}}, {title: 'othertitle', src: "https://static.thenounproject.com/png/180007-200.png", alt: "sub", post: {}}]}/> 
+  </Col>
+  <Col>
+    <InputField mt="10"/>
+    <DisplayPosts />
+  </Col>
+</Row>
+</Container> */}
--- src/features/Components/Sidebar/Sidebar.js
+++ src/features/Components/Sidebar/Sidebar.js
@@ -4,45 +4,118 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import ToggleButtonGroup from "react-bootstrap/ToggleButtonGroup";
 import ToggleButton from "react-bootstrap/ToggleButton";
+import SidebarMenu from 'react-bootstrap-sidebar-menu';
 
 export function Sidebar ({ shortcuts }) {
   //props will be either homeLinks or postLinks => arrays of objects
-  //homelinks example: [{title: example, src: src, alt: altText, post: null}, {...}]
-  //postLinks example: [{title: title, src: src, alt: altText, post: {comments: {}, postNum: 1}}, {...}]
+  //homelinks example: [{
+  //   title: example, 
+  //   src: src, 
+  //   alt: altText, 
+  //   post: null
+  // }, {
+  //   ...
+  // }]
+  //postLinks example: [{
+    // title: title, 
+    // src: src, 
+    // alt: altText, 
+    // post: {comments: {}, postNum: 1}
+  // }, {
+  //   ...
+  // }]
 
 return (
-  <Tab.Container id="home-page">
-    <Row className="flex-xs-grow-0 flex-xs-shrink-0">
-      <Col className="border">
-        <ToggleButtonGroup 
-          
-          name="home" 
-          vertical="true">
-          {shortcuts.map((shortcut, id) => {
-            if (shortcut.post === null) {
-              return (
-                <ToggleButton 
-                  className="my-3 px-0" 
-                  variant="" 
-                  key={id}>
+  <SidebarMenu 
+    className="flex-container component"
+    expand='lg' 
+    hide='md'
+    exclusiveExpand={false}
+    collapseOnSelect={false} >
+    <SidebarMenu.Collapse>
+      <SidebarMenu.Header>
+        <SidebarMenu.Brand>
+          <img className="icon" src="https://cdn-icons-png.flaticon.com/512/427/427533.png"/>
+        </SidebarMenu.Brand>
+        <SidebarMenu.Toggle/>
+      </SidebarMenu.Header>
+      <SidebarMenu.Body>
+        <SidebarMenu.Nav>
+          <SidebarMenu.Nav.Link>
+            <SidebarMenu.Nav.Icon>
+              <img 
+                className="icon" 
+                src={shortcuts[0].src} 
+                alt={shortcuts[0].alt}/>
+            </SidebarMenu.Nav.Icon>
+            <SidebarMenu.Nav.Title>
+              <p>Help</p>
+            </SidebarMenu.Nav.Title>
+          </SidebarMenu.Nav.Link>
+        </SidebarMenu.Nav>
+        <SidebarMenu.Sub>
+          <SidebarMenu.Sub.Toggle>
+            <SidebarMenu.Nav.Icon>
+              <img 
+                className="icon" 
+                src={shortcuts[1].src} 
+                alt={shortcuts[1].alt}/>
+            </SidebarMenu.Nav.Icon>
+            <SidebarMenu.Nav.Title>
+              <p>Search</p>
+            </SidebarMenu.Nav.Title>
+          </SidebarMenu.Sub.Toggle>
+          <SidebarMenu.Sub.Collapse>
+            <SidebarMenu.Nav>
+              <SidebarMenu.Nav.Link>
+                <SidebarMenu.Nav.Icon>
                   <img 
-                    height={40} 
-                    width={40} 
-                    src={shortcut.src} 
-                    alt={shortcut.alt} />
-                </ToggleButton> 
-              )
-            } else {
-            return (
-              <ToggleButton className="my-3" key={id}>
-                <p>{shortcut.title}</p>
-              </ToggleButton>
-            )
-            }
-          })}
-        </ToggleButtonGroup>
-      </Col>
-    </Row>
-  </Tab.Container>
-  )
-}
+                    className="icon" 
+                    src={shortcuts[1].src} 
+                    alt={shortcuts[1].alt}/>
+                </SidebarMenu.Nav.Icon>
+                <SidebarMenu.Nav.Title>
+                  <p>Surprise</p>
+                </SidebarMenu.Nav.Title>
+              </SidebarMenu.Nav.Link>
+            </SidebarMenu.Nav>
+          </SidebarMenu.Sub.Collapse>
+        </SidebarMenu.Sub>
+      </SidebarMenu.Body>
+    </SidebarMenu.Collapse>
+  </SidebarMenu>
+)}
+
+
+// <Tab.Container id="home-page">
+//   <Row className="flex-xs-grow-0 flex-xs-shrink-0">
+//     <Col className="border">
+//       <ToggleButtonGroup 
+//         name="home" 
+//         vertical="true">
+//         {shortcuts.map((shortcut, id) => {
+//           if (shortcut.post === null) {
+//             return (
+//               <ToggleButton 
+//                 className="my-3 px-0" 
+//                 variant="" 
+//                 key={id}>
+//                 <img 
+//                   height={40} 
+//                   width={40} 
+//                   src={shortcut.src} 
+//                   alt={shortcut.alt} />
+//               </ToggleButton> 
+//             )
+//           } else {
+//           return (
+//             <ToggleButton className="my-3" key={id}>
+//               <p>{shortcut.title}</p>
+//             </ToggleButton>
+//           )
+//           }
+//         })}
+//       </ToggleButtonGroup>
+//     </Col>
+//   </Row>
+// </Tab.Container>
