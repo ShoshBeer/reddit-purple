b67206b3cb2927818383fd2ac999a90468e19d7d
JeanJoblin <invisibleorbs@gmail.com>
2022-12-12 11:26:52 -0500

    Make Posts display subreddit, title and upvotes

--- src/features/Components/DisplayPosts/DisplayPosts.js
+++ src/features/Components/DisplayPosts/DisplayPosts.js
@@ -1,25 +1,54 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Post } from "../Post/Post";
-import { fetchURLData, loading, loadJSON, loadTitles, selectFoundPosts, selectTitles, } from "./postDataSlice"
+import {
+        fetchURLData, 
+        loading, 
+        loadJSON, 
+        loadTitles, 
+        selectComments,
+        selectFoundPosts,
+        selectSubs,
+        selectTitles,
+        selectUpvotes,
+    } from "./postDataSlice"
 import { selectLinks } from '../InputField/InputFieldSlice'
 
 export function DisplayPosts() {
     const dispatch = useDispatch();
-    const linkList = useSelector(selectLinks)
-    const titles = useSelector(selectTitles);
-    const foundPosts = useSelector(selectFoundPosts);
-    const isLoading = useSelector(loading);
+    const linkList = useSelector(selectLinks);
 
     useEffect(() => {
         dispatch(loadJSON(linkList));
         dispatch(fetchURLData());
     }, [linkList]);
 
+    const titles = useSelector(selectTitles);
+    const subs = useSelector(selectSubs);
+    const upvotes = useSelector(selectUpvotes);
+    const comments = useSelector(selectComments);
+    
+
+    //remove this const, and change mapped array in <ul> to linkList get all
+    const displayLinks = linkList.slice(0, 5);
+
+
+
     return (
         <div className="post-container">
             <ul>
-                { linkList.map((link, ind) => <Post post={link} key={ind} title={titles[ind]} />)}
+                { displayLinks.map((link, ind) => {
+                    return ( 
+                        <Post 
+                            post={link} 
+                            key={ind}
+                            title={titles[ind]}
+                            sub={subs[ind]}
+                            score={upvotes[ind]}
+                            comment={comments[ind]}
+                        />
+                    );
+                })}
             </ul>
         </div>
     )
