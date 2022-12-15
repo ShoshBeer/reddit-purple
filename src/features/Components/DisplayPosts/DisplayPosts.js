a48b23018884ad38854f58848c36b3c9e9219b00
ShoshBeer <shoshanabeer@gmail.com>
2022-12-15 11:30:20 -0500

    refactor DisplayPosts, postDataSlice and handle error responses

--- src/features/Components/DisplayPosts/DisplayPosts.js
+++ src/features/Components/DisplayPosts/DisplayPosts.js
@@ -1,56 +1,50 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Post } from "../Post/Post";
 import {
         fetchURLData, 
-        loading, 
-        loadJSON, 
-        loadTitles, 
-        selectComments,
-        selectFoundPosts,
-        selectSubs,
-        selectTitles,
-        selectUpvotes,
+        loadJSON,
+        selectLoading, 
+        selectPostObjects,
     } from "./postDataSlice"
 import { selectLinks } from '../InputField/InputFieldSlice'
 
 export function DisplayPosts() {
     const dispatch = useDispatch();
     const linkList = useSelector(selectLinks);
+    const postObjects = useSelector(selectPostObjects);
+    const loading = useSelector(selectLoading);
 
-    const test = ['https://www.reddit.com/r/WhitePeopleTwitter/comments/zlnhkq/elon_musk_has_suspended_the_twitter_account_that/', 'https://github.com/ivp-dev/react-bootstrap-sidebar-menu/issues/1'];
+    // const test = [
+    //   'https://www.reddit.com/r/WhitePeopleTwitter/comments/zlnhkq/elon_musk_has_suspended_the_twitter_account_that/',  
+    //   'https://www.reddit.com/r/AskReddit/comments/9wsvhk/what_is_the_best_post_of_reddit_of_all_time/', 
+    //   'https://www.reddit.com/r/todayilearned/comments/zmgqby/til_that_in_2011_a_russian_wouldbe_suicide_bomber/the',
+    //   'https://www.reddit.com/r/todayilearned/comments/zm04zt/til_pacman_grossed_1_billion_in_quarters_in_its/', 
+    //   'https://zombo.com/'];
 
     useEffect(() => {
-        dispatch(loadJSON(test));
+        dispatch(loadJSON(linkList));
         dispatch(fetchURLData());
-    }, [linkList]);
-
-    const titles = useSelector(selectTitles);
-    const subs = useSelector(selectSubs);
-    const upvotes = useSelector(selectUpvotes);
-    const comments = useSelector(selectComments);
-    
+    }, [dispatch, linkList]);
 
     //remove this const, and change mapped array in <ul> to linkList get all
-    const displayLinks = linkList.slice(0, 5);
-
-
+    const displayLinks = postObjects.slice(0, 5);
 
     return (
         <div className="post-container">
             <ul>
-                { displayLinks.map((link, ind) => {
+                { loading ? <p>Loading</p> : postObjects.length === 0 ? <div></div> : displayLinks.map((post, ind) => {
                     return ( 
                         <Post 
-                            post={link} 
+                            post={post.link} 
                             key={ind}
-                            title={titles[ind]}
-                            sub={subs[ind]}
-                            score={upvotes[ind]}
-                            comment={comments[ind]}
+                            title={post.title}
+                            sub={post.subreddit_name_prefixed}
+                            score={post.score}
+                            comment={post.comments}
                         />
                     );
-                })}
+                  })}
             </ul>
         </div>
     )
