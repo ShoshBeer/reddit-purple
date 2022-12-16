8f7055827bd73ba00c55043c9e7a487754a962b0
ShoshBeer <shoshanabeer@gmail.com>
2022-12-16 15:34:58 -0500

    Add more post data and create basic card layout for FullPosts

--- src/features/Components/FullPost/FullPost.js
+++ src/features/Components/FullPost/FullPost.js
@@ -1,14 +1,18 @@
 import { Container, Row, Col, Card, Button} from "react-bootstrap";
 
-export function FullPost (post) {
+export function FullPost ({ title, body, comments, score, date, sub }) {
   return (
-    <Card>
+    <Card className="text-left">
+      <Card.Header>
+        <p>{sub}</p>
+        <p>{new Date(date*1000).toString()}</p>
+      </Card.Header>
       <Card.Body>
         <Card.Title>
-          {post.title}
+          {title}
         </Card.Title>
-        <Card.Text>
-          {post.body}
+        <Card.Text className="post-body">
+          {body}
         </Card.Text>
       </Card.Body>
     </Card>
