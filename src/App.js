5e3a0b0c844dedb33f01624ed2e69883d669bf7b
JeanJoblin <95894506+JeanJoblin@users.noreply.github.com>
2023-01-13 11:18:31 -0500

    Implement router (#11)
    * Start using react router
    
    * really start using react router
    
    * Clean up imports/other junk
    
    * Make input submit like a form
    
    * make submit button inline with input field

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
-import { DisplayPosts } from './features/Components/DisplayPosts/DisplayPosts';
-import { InputField } from './features/Components/InputField/InputField';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { Link } from "react-router-dom";
+import {
+  Outlet,
+} from "react-router-dom";
 
 function App() {
   return (
@@ -13,11 +15,13 @@ function App() {
         <Col>
           <h1>Welcome to the Museum of Reddit Natural History!</h1>
           <p>Use this site to browse Reddit posts linked in the comments of another post!</p>
-          <p>Pop this link below to see what happens: https://www.reddit.com/r/AskReddit/comments/9wsvhk/what_is_the_best_post_of_reddit_of_all_time/</p>
+          <p>Pop this link below to see what happens:</p>
+          <p>https://www.reddit.com/r/AskReddit/comments/9wsvhk/what_is_the_best_post_of_reddit_of_all_time/</p>
         </Col>
       </Row>
-      <InputField mt="10"/>
-      <DisplayPosts />
+      <Row>
+        <Outlet />
+      </Row>
     </Container>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,23 +1,45 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import { InputField } from './features/Components/InputField/InputField';
+import { DisplayPosts } from './features/Components/DisplayPosts/DisplayPosts';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectLinks } from './features/Components/InputField/InputFieldSlice';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: "",
+        element: <InputField mt="10"/>
+      },
+      {
+        path: "display",
+        element: <DisplayPosts />,
+        },
+    ],
+  },
+]);
 
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
+    </React.StrictMode>
+
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
