6bc1a0544ceefec9fb3310d7c2ef2e0f73392171
ShoshBeer <shoshanabeer@gmail.com>
2023-05-20 16:53:30 -0400

    move logic into content, can't import displayposts

--- public/app/background.js
+++ public/app/background.js
@@ -9,13 +9,6 @@ let linkList;
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     createDisplayFromLinks(request).then(sendResponse);
-    // console.log("Received the link list from Content.js");
-    // console.log(request.message.length);
-    // linkList = request.message;
-    // console.log(`Inside message? ${linkList[4]}`);
-    // if (request.message.length > 3) {
-    //   sendResponse({farewell: `${request.message.length} links passed to the app :)`});
-    // }
     return true;
   }
 );
